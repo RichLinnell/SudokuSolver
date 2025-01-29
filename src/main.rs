8f6a1ecbbd32ec4d581a42d68d406446fe6fc38f
Rich Linnell <richlinnell@gmail.com>
2025-01-29 22:27:19 +0000

    Introduced eFrame and egui to solve the UI problems

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,49 @@
 mod grid;
-use grid::Grid; 
+use eframe::egui;
+use grid::Grid;
 use std::io;
 
-fn main() {
-    let mut main_grid = Grid::new();
-    // prompt user to add starting cells - TODO work out the format for this that works for entry
-    let mut cell_value = String::new();
-    println!("Enter value x");
-    io::stdin().read_line(&mut cell_value).expect("Failed to read input from user.");
-    println!("YOu entered {cell_value}");
-    let cell_value_int: i32 = cell_value.trim().parse().expect("Failed to parse the value from the entry.");
+// TODO: I've imported eFrame and eGui libraries.  I now need to work out the format of the screen
+// I'll be showing the user.  It's going to be a Sudoku grid, and I suspect I want to allow the
+// user to move around the cells and add entries.
+// Then I will want some buttons at the bottom :
+//  - Clear Grid
+//  - Solve
+//  - Exit
+//  We'll also need a way to indicate issues - i.e. "This is an unsolvable Grid" and the like
+//  We'll also need a way to indicate duplicate values on the grid - rows with 2 numbers the same
+//  and the like.
 
-    // output the starting grid to the screen:
-    main_grid.set_cell(0, 0, cell_value_int);
-    //   TODO : make a render method for the grid
-    main_grid.print_grid();
-    // begin main calc cycle
-    // use the "solver" methods to solve the grid
-    // if we found a solution, output the solution to the screen
-    // if we didn't find a solution, output that we didn't find a solution
+fn main() -> eframe::Result {
+    // Set up the main grid
+    let mut main_grid = Grid::new();
+
+    // this code, as it should move out to another module.
+    // Demo form code for the egui framework.
+    let mut name = "Test".to_owned();
+    let mut age = 55;
+    // Form size.
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([620.0, 440.0]),
+        ..Default::default()
+    };
+    eframe::run_simple_native(
+        "Sudoku Solving App - Built in Rust.",
+        options,
+        move |ctx, _frame| {
+            egui::CentralPanel::default().show(ctx, |ui| {
+                ui.heading("Sudoku Solving Application, built in Rust");
+                ui.horizontal(|ui| {
+                    let name_label = ui.label("Your name: ");
+                    ui.text_edit_singleline(&mut name)
+                        .labelled_by(name_label.id);
+                });
+                ui.add(egui::Slider::new(&mut age, 0..=120).text("age"));
+                if ui.button("Increment").clicked() {
+                    age += 1;
+                }
+                ui.label(format!("Hello '{name}', age {age}"));
+            });
+        },
+    )
 }
